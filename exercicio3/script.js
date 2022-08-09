757a4245c7f4e2e32fe5e5d30c2ce24a64709f77
Lucas <lc.lucas070@gmail.com>
2022-08-09 18:49:48 -0300

    Resolucao dos exercicios A013 da turma barbosa D

--- exercicio3/script.js
+++ exercicio3/script.js
@@ -1,13 +1,24 @@
 //código a ser reescrito
 
-if(nome === "José"){
-	console.log("Oi, Zé!");
-} else {
-	console.log("Olá, "+nome);
-}
+// if(nome === "José"){
+// 	console.log("Oi, Zé!");
+// } else {
+// 	console.log("Olá, "+nome);
+// }
 
-if(idade >= 18){
-	console.log("pode tirar carteira de motorista!");
-} else {
-	console.log("Ainda não pode tirar carteira de motorista");
-}
+const nome = prompt('Digite seu nome')
+
+console.log(nome === 'José'? `Oi, Zé!` : `Olá, ${nome}`)
+
+
+// if(idade >= 18){
+// 	console.log("pode tirar carteira de motorista!");
+// } else {
+// 	console.log("Ainda não pode tirar carteira de motorista");
+// }
+
+const idade = prompt('Digite sua idade');
+
+console.log(
+	idade >= 18 ? 'Pode tirar carteira de motorista!'
+	: 'Ainda não pode tirar carteira de motorista')
